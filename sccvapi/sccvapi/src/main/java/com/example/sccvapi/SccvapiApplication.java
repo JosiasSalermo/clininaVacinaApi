d9aedf995ee540b94db1668a917b2b089440d971
Josias <josiassalermojunior>
2024-02-22 10:20:13 -0300

    iniciando projeto

---
+++ sccvapi/sccvapi/src/main/java/com/example/sccvapi/SccvapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sccvapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SccvapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SccvapiApplication.class, args);
+	}
+
+}
---
+++ sccvapi/sccvapi/src/test/java/com/example/sccvapi/SccvapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sccvapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SccvapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
