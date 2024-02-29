ad7f6e576d95466ef7b3ed8c0bca3432b19e12e9
Josias <josiassalermojunior>
2024-02-29 08:07:56 -0300

    conferindo codigo

--- sccvapi/sccvapi/src/main/java/com/example/sccvapi/model/entity/Paciente.java
+++ sccvapi/sccvapi/src/main/java/com/example/sccvapi/model/entity/Paciente.java
@@ -1,5 +1,17 @@
 package com.example.sccvapi.model.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Paciente extends PessoaFisica{
 
     private String tipoSanguineo;
