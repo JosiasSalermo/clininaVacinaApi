ce2e79277b06f26fabe66df2578f8b658716a848
Josias <josiassalermojunior>
2024-03-05 08:30:53 -0300

    Funcionario

--- sccvapi/sccvapi/src/main/java/com/example/sccvapi/model/entity/Funcionario.java
+++ sccvapi/sccvapi/src/main/java/com/example/sccvapi/model/entity/Funcionario.java
@@ -1,11 +1,11 @@
 package com.example.sccvapi.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
 
 @Entity
 @Data
