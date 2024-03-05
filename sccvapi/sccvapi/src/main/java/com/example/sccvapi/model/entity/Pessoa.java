68a72d9c9266ac742529d144ea66acd2b89d09a8
Josias <josiassalermojunior>
2024-03-05 08:35:25 -0300

    Pessoa

--- sccvapi/sccvapi/src/main/java/com/example/sccvapi/model/entity/Pessoa.java
+++ sccvapi/sccvapi/src/main/java/com/example/sccvapi/model/entity/Pessoa.java
@@ -1,13 +1,15 @@
 package com.example.sccvapi.model.entity;
 
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
 @Data
 @NoArgsConstructor
