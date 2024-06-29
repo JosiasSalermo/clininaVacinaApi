f973a85789ca076eaca3c3539d27878448444388
Josias <josiassalermojunior>
2024-06-29 07:41:25 -0300

    GetMapping CondicaoEspeciaController

--- sccvapi/sccvapi/src/main/java/com/example/sccvapi/api/controller/CondicaoEspecialController.java
+++ sccvapi/sccvapi/src/main/java/com/example/sccvapi/api/controller/CondicaoEspecialController.java
@@ -2,9 +2,40 @@ package com.example.sccvapi.api.controller;
 
 import com.example.sccvapi.api.dto.CondicaoEspecialDTO;
 import com.example.sccvapi.model.entity.CondicaoEspecial;
-import org.modelmapper.ModelMapper;
+import com.example.sccvapi.service.*;
 
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/condicaoEspecial")
+@RequiredArgsConstructor
+@CrossOrigin
 public class CondicaoEspecialController {
+
+    private final CondicaoEspecialService service;
+
+    @GetMapping()
+    public ResponseEntity get(){
+        List<CondicaoEspecial> condicoesEspeciais = service.getCondicoesEspeciais();
+        return ResponseEntity.ok(condicoesEspeciais.stream().map(CondicaoEspecialDTO::create).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity get(@PathVariable("id") Long id){
+        Optional<CondicaoEspecial> condicaoEspecial = service.getCondicaoEspecialById(id);
+        if(!condicaoEspecial.isPresent()){
+            return new ResponseEntity("Condição Especial não encontrada", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(condicaoEspecial.map(CondicaoEspecialDTO::create));
+    }
 
     public CondicaoEspecial converter(CondicaoEspecialDTO dto){
         ModelMapper modelMapper = new ModelMapper();
