46d06bb5637c11cc66c980f7c85a8b4653d161e2
Josias <josiassalermojunior>
2024-07-01 06:10:11 -0300

    GetMapping da classe PacienteController

--- sccvapi/sccvapi/src/main/java/com/example/sccvapi/api/controller/PacienteController.java
+++ sccvapi/sccvapi/src/main/java/com/example/sccvapi/api/controller/PacienteController.java
@@ -1,10 +1,42 @@
 package com.example.sccvapi.api.controller;
 
-import com.example.sccvapi.api.dto.PacienteDTO;
-import com.example.sccvapi.model.entity.Paciente;
-import org.modelmapper.ModelMapper;
+import com.example.sccvapi.api.dto.*;
+import com.example.sccvapi.model.entity.*;
+import com.example.sccvapi.service.*;
 
+import com.example.sccvapi.exception.RegraNegocioException;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/paciente")
+@RequiredArgsConstructor
+@CrossOrigin
 public class PacienteController {
+
+    private final PacienteService service;
+
+    @GetMapping()
+    public ResponseEntity get(){
+        List<Paciente> pacientes = service.getPacientes();
+        return ResponseEntity.ok(pacientes.stream().map(PacienteDTO::create).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity get(@PathVariable("id") Long id){
+        Optional<Paciente> paciente = service.getPacienteById(id);
+        if(!paciente.isPresent()){
+            return new ResponseEntity("Paciente não encontrado", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(paciente.map(PacienteDTO::create));
+    }
 
     public Paciente converter(PacienteDTO dto){
         ModelMapper modelMapper = new ModelMapper();
