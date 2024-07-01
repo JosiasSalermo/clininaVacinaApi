d5c50e66d822e8df67f6dd3a54dfad60dca8281e
Josias <josiassalermojunior>
2024-07-01 05:53:32 -0300

    GetMapping da classe FornecedorController

--- sccvapi/sccvapi/src/main/java/com/example/sccvapi/api/controller/FornecedorController.java
+++ sccvapi/sccvapi/src/main/java/com/example/sccvapi/api/controller/FornecedorController.java
@@ -1,10 +1,42 @@
 package com.example.sccvapi.api.controller;
 
-import com.example.sccvapi.api.dto.FornecedorDTO;
-import com.example.sccvapi.model.entity.Fornecedor;
-import org.modelmapper.ModelMapper;
+import com.example.sccvapi.api.dto.*;
+import com.example.sccvapi.model.entity.*;
+import com.example.sccvapi.service.*;
 
+import com.example.sccvapi.exception.RegraNegocioException;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/fornecedor")
+@RequiredArgsConstructor
+@CrossOrigin
 public class FornecedorController {
+
+    private final FornecedorService service;
+
+    @GetMapping()
+    public ResponseEntity get(){
+        List<Fornecedor> fornecedores = service.getFornecedores();
+        return ResponseEntity.ok(fornecedores.stream().map(FornecedorDTO::create).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity get(@PathVariable("id") Long id){
+        Optional<Fornecedor> fornecedor = service.getFornecedorById(id);
+        if(!fornecedor.isPresent()){
+            return new ResponseEntity("Fornecedor não encontrado", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(fornecedor.map(FornecedorDTO::create));
+    }
 
     public Fornecedor converter(FornecedorDTO dto){
         ModelMapper modelMapper = new ModelMapper();
