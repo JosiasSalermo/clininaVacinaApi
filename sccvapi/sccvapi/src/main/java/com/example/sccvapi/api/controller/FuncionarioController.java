dbdb1250f640453d4608e1947bdeecb9e40421a0
Josias <josiassalermojunior>
2024-07-01 05:59:27 -0300

    GetMapping da classe FuncionarioController

--- sccvapi/sccvapi/src/main/java/com/example/sccvapi/api/controller/FuncionarioController.java
+++ sccvapi/sccvapi/src/main/java/com/example/sccvapi/api/controller/FuncionarioController.java
@@ -1,10 +1,42 @@
 package com.example.sccvapi.api.controller;
 
-import com.example.sccvapi.api.dto.FuncionarioDTO;
-import com.example.sccvapi.model.entity.Funcionario;
-import org.modelmapper.ModelMapper;
+import com.example.sccvapi.api.dto.*;
+import com.example.sccvapi.model.entity.*;
+import com.example.sccvapi.service.*;
 
+import com.example.sccvapi.exception.RegraNegocioException;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/funcionario")
+@RequiredArgsConstructor
+@CrossOrigin
 public class FuncionarioController {
+
+    private final FuncionarioService service;
+
+    @GetMapping()
+    public ResponseEntity get(){
+        List<Funcionario> funcionarios = service.getFuncionarios();
+        return ResponseEntity.ok(funcionarios.stream().map(FuncionarioDTO::create).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity get(@PathVariable("id") Long id){
+        Optional<Funcionario> funcionario = service.getFuncionarioById(id);
+        if(!funcionario.isPresent()){
+            return new ResponseEntity("Funcionario não encontrado", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(funcionario.map(FuncionarioDTO::create));
+    }
 
     public Funcionario converter(FuncionarioDTO dto){
         ModelMapper modelMapper = new ModelMapper();
