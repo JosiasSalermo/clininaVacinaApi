0dae22c10490057eea219a359bec16368f0ff7bf
Josias <josiassalermojunior>
2024-07-01 06:05:23 -0300

    GetMapping da classe LoteController

--- sccvapi/sccvapi/src/main/java/com/example/sccvapi/api/controller/LoteController.java
+++ sccvapi/sccvapi/src/main/java/com/example/sccvapi/api/controller/LoteController.java
@@ -1,10 +1,42 @@
 package com.example.sccvapi.api.controller;
 
-import com.example.sccvapi.api.dto.LoteDTO;
-import com.example.sccvapi.model.entity.Lote;
-import org.modelmapper.ModelMapper;
+import com.example.sccvapi.api.dto.*;
+import com.example.sccvapi.model.entity.*;
+import com.example.sccvapi.service.*;
 
+import com.example.sccvapi.exception.RegraNegocioException;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/lote")
+@RequiredArgsConstructor
+@CrossOrigin
 public class LoteController {
+
+    private final LoteService service;
+
+    @GetMapping()
+    public ResponseEntity get(){
+        List<Lote> lotes = service.getLotes();
+        return ResponseEntity.ok(lotes.stream().map(LoteDTO::create).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity get(@PathVariable("id") Long id){
+        Optional<Lote> lote = service.getLoteById(id);
+        if(!lote.isPresent()){
+            return new ResponseEntity("Lote não encontrado", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(lote.map(LoteDTO::create));
+    }
 
     public Lote converter(LoteDTO dto){
         ModelMapper modelMapper = new ModelMapper();
