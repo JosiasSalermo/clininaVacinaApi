5f88891d978e40cc1ba4e7b5b9c2be5e79ecac9a
Josias <josiassalermojunior>
2024-07-02 05:16:31 -0300

    GetMapping da classe Vacinacao

--- sccvapi/sccvapi/src/main/java/com/example/sccvapi/api/controller/VacinacaoController.java
+++ sccvapi/sccvapi/src/main/java/com/example/sccvapi/api/controller/VacinacaoController.java
@@ -1,10 +1,42 @@
 package com.example.sccvapi.api.controller;
 
-import com.example.sccvapi.api.dto.VacinacaoDTO;
-import com.example.sccvapi.model.entity.Vacinacao;
-import org.modelmapper.ModelMapper;
+import com.example.sccvapi.api.dto.*;
+import com.example.sccvapi.model.entity.*;
+import com.example.sccvapi.service.*;
 
+import com.example.sccvapi.exception.RegraNegocioException;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/vacinacao")
+@RequiredArgsConstructor
+@CrossOrigin
 public class VacinacaoController {
+
+    private final VacinacaoService service;
+
+    @GetMapping()
+    public ResponseEntity get(){
+        List<Vacinacao> vacinacoes = service.getVacinacoes();
+        return ResponseEntity.ok(vacinacoes.stream().map(VacinacaoDTO::create).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity get(@PathVariable("id") Long id){
+        Optional<Vacinacao> vacinacao = service.getVacinacaoById(id);
+        if(!vacinacao.isPresent()){
+            return new ResponseEntity("Vacinacao não encontrada", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(vacinacao.map(VacinacaoDTO::create));
+    }
 
     public Vacinacao converter(VacinacaoDTO dto){
         ModelMapper modelMapper = new ModelMapper();
