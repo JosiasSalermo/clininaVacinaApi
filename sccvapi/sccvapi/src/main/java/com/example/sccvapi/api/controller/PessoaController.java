16fb2a8f82bc8058926567b81fc80e3d791eac6b
Josias <josiassalermojunior>
2024-07-02 04:37:02 -0300

    GetMapping da classe PessoaController

--- sccvapi/sccvapi/src/main/java/com/example/sccvapi/api/controller/PessoaController.java
+++ sccvapi/sccvapi/src/main/java/com/example/sccvapi/api/controller/PessoaController.java
@@ -23,6 +23,21 @@ public class PessoaController {
 
     private final PessoaService service;
 
+    @GetMapping()
+    public ResponseEntity get(){
+        List<Pessoa> pessoas = service.getPessoas();
+        return ResponseEntity.ok(pessoas.stream().map(PessoaDTO::create).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity get(@PathVariable("id") Long id){
+        Optional<Pessoa> pessoa = service.getPessoaById(id);
+        if(!pessoa.isPresent()){
+            return new ResponseEntity("Pessoa não encontrada", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(pessoa.map(PessoaDTO::create));
+    }
+
     public Pessoa converter(PessoaDTO dto){
         ModelMapper modelMapper = new ModelMapper();
         Pessoa pessoa = modelMapper.map(dto, Pessoa.class);
